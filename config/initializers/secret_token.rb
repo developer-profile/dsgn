066b29163a71aaddfd2921bd422d505cc44f2f94
molokows <d@moloko.ws>
2013-06-18 21:40:58 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DsgnPw::Application.config.secret_token = '9df16879aef6b0fc746d94e38ef6b5ae31b0d74865d03140af220077e35f25a888dd8de825057b7758738250d8cf033abc767969123ff04cc31efbdbe3f1cb9f'
